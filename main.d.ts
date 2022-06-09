229385f36a609031907e425308f78bb17885871a
Sergey Sedoy <sergey.sedoy.97@icloud.com>
2022-06-09 13:24:49 +0300

    chore: use template
    Release-As: 2.0.2

---
+++ main.d.ts
@@ -0,0 +1,80 @@
+/// <reference types="node" />
+export const LOCALHOST_CERT: '-----BEGIN CERTIFICATE-----\nMIIDlTCCAn2gAwIBAgIJAIiA2UzhbmG1MA0GCSqGSIb3DQEBCwUAMGMxCzAJBgNV\nBAYTAlJVMQ8wDQYDVQQIEwZNb3Njb3cxDzANBgNVBAcTBk1vc2NvdzEeMBwGA1UE\nChMVIEF3ZXNvbWUgT3JnYW5pemF0aW9uMRIwEAYDVQQDEwlsb2NhbGhvc3QwHhcN\nMjEwNDA4MTMyNDMwWhcNMzEwNDA5MTMyNDMwWjBjMQswCQYDVQQGEwJSVTEPMA0G\nA1UECBMGTW9zY293MQ8wDQYDVQQHEwZNb3Njb3cxHjAcBgNVBAoTFSBBd2Vzb21l\nIE9yZ2FuaXphdGlvbjESMBAGA1UEAxMJbG9jYWxob3N0MIIBIjANBgkqhkiG9w0B\nAQEFAAOCAQ8AMIIBCgKCAQEA16RGziyQFoHtUTcboct0NC5FiNZVw67S8/Ti5Iil\nWJQMF3GV8tL4o42lN7PoexMzgU9tYo4vvgtWGWS+8W7luw9LiAaqjNjKr6lOE8go\n8P7eQckOskt8bDorIFtPKUUJtwFgn0DEbotzRLjc4SBUEkwyb+n1Pli9rKxOuMX0\nZjz2HtSa1tMfp1Fg0FCgJS6MgUL9g3e2Mf7mcW5vsfF4jgUzZHJXfsfopr+nu0Ds\n2EvQfrFs3UVo76/PCHJPTIzkfgCgCXjgCjhHbwX1j0KqsiAbGjqzgeFe3KmzkUGS\nrJAcpIHhdDVtyDqOeB/mwvqFBvx51pi0afwR4j03lQyM/QIDAQABo0wwSjAJBgNV\nHRMEAjAAMBEGCWCGSAGG+EIBAQQEAwIE8DALBgNVHQ8EBAMCBaAwHQYDVR0lBBYw\nFAYIKwYBBQUHAwIGCCsGAQUFBwMBMA0GCSqGSIb3DQEBCwUAA4IBAQBul441QXh8\nmlCuyQ/1qB0p5X64hIHc02bLvUgajSMXNO4Nwt0T48z2Qh4kKjiya8/Uiu5O8JH1\nQ0qvKt4VIAUmEKsI0f+N4WkgFbxSdPR6Ns9s7NtfG7ttaKHcF0+yss2GKa2ZNBl4\nn4pMW4rl9k0raUjvoNIkNkmVgEP4Wj7WxteWID1pSj7J7HywJT8Qxf+yAic0ic6s\nskFrJ7PTdzOHkEi1GSr8S/4N37kMq6u8oZLR35nuKFS8OVRRSuKmqY4iyVTZoCqS\nRPFA2Q4jEzgvVWarYQ16YyifB8mnZipztUMsA1TceIQ9fbYKlBUo9EENWpF4itFI\n1OgQXUsQmCyg\n-----END CERTIFICATE-----'
+export const LOCALHOST_KEY: '-----BEGIN RSA PRIVATE KEY-----\nMIIEpgIBAAKCAQEA16RGziyQFoHtUTcboct0NC5FiNZVw67S8/Ti5IilWJQMF3GV\n8tL4o42lN7PoexMzgU9tYo4vvgtWGWS+8W7luw9LiAaqjNjKr6lOE8go8P7eQckO\nskt8bDorIFtPKUUJtwFgn0DEbotzRLjc4SBUEkwyb+n1Pli9rKxOuMX0Zjz2HtSa\n1tMfp1Fg0FCgJS6MgUL9g3e2Mf7mcW5vsfF4jgUzZHJXfsfopr+nu0Ds2EvQfrFs\n3UVo76/PCHJPTIzkfgCgCXjgCjhHbwX1j0KqsiAbGjqzgeFe3KmzkUGSrJAcpIHh\ndDVtyDqOeB/mwvqFBvx51pi0afwR4j03lQyM/QIDAQABAoIBAQCuyPibJIOluqog\nqgo7mi0WHms9/nyFn65dDqDZm+hpY5ZpaiegKmBeMPE5tRk6qNWWekqvF5Ca+ZVP\n/9jE2J5cgIk4OC8E+rNOrmwanKKStAJyUAUZfxXao0tRbrE7Qjodm4A1lMmi0GUd\nzrk5wHpkWl6HV5rwbf1PeFpWah0uv73aC00zweXzJVoCq+eZChhpmabzLRO+slUy\n7VACzpwgJnPeuIJGNzXE9jCExzwRkl/auFm4wM5c9WXehpTB3EwYVtJfoMlgu4KE\nQh9TlngtBZt5/OYPZbxatMve5gbOfCq1UJlWntEGi1AJd+jBLIeYHdgO3mstq3QH\nzwDel1ABAoGBAOxDs7nWZQ4ocK+g/Tm5227rVoDWqrfmIyAOaNE8hiVJhahtXoyq\n4W6cJtGHLvWc9AXxU4LqWs0XKVNbG+xeVlaro8od1X/Z/IY02lSjW0dOXhbsvPVw\nnIKErk2Wr70d4aQoQ37x6wYU+xJcapQn7iqcXgm7CDHkWjbKvIVx5cxFAoGBAOmn\nk21X884AXYtc8goseR9+28B39kpEvDgE8AOzk0XHkfPOxF2l+Z50fILzVr4NURNv\nBhDiIhexVgueoHjJ49OVH95LUISThojI8/BkjPXg/h60d7pOlyfoZowhMRzcZHq2\n3mQrqJhArrh2CoRL/JSwJP3FjrAcp9yyA24P6XVZAoGBAKmDZbbnIThIQZlxK144\ngD0T8subuX8aSodcb034W9Ly7kfKuLS6geXneV6J3GJyyw5ceGuMk7tka80XqHAt\nu0qR+YExaJZDo4/y6dciIYKGsrFGB8kdk919LsYSYGKSxusNzGePUO3bLcydrAZC\no/nEmR/oJlgNm8CGMz6XWqX1AoGBAK5BF6X4bg84HouM6cXEnSBsD59e2ANTd7uf\nkxBvoGnuCF932OKuoZcW8LUInaxnagvARRnaS+q3iqBn0O3EQ3DMSlQSfx4gl7jz\nhVnG44mMHnjvxkrfycMtgy0GpAYOJ7GNKBY0qSvDMYrIHdfEg76wDyZja6LT/CyP\nZhdzLn0hAoGBAItbaHy1T0UMD07ocKO/1bgzvPozbAiIF/L4TvW5Vt21Uvv4Cbq2\nVdn937sOFHGPVbRUJ8Y9KOhhl7iaO4WH9EdxSPTEMWHEbsZoaKgvgtAT/sYAIKfp\nUJeqNxeR9YN60iLw6jJpM2mq11sa4pnGh53ISP1uWkqVLMgXIvJPFzuQ\n-----END RSA PRIVATE KEY-----'
+export function http({
+  createOptions,
+  handlers,
+  listenOptions,
+  wait,
+}: {
+  createOptions?: {}
+  handlers?: {}
+  listenOptions?: {}
+  wait?: boolean
+}): Promise<import('http').Server>
+export function https({
+  createOptions,
+  handlers,
+  listenOptions,
+  wait,
+}: {
+  createOptions?: {}
+  handlers?: {}
+  listenOptions?: {}
+  wait?: boolean
+}): Promise<import('https').Server>
+export function http2({
+  createOptions,
+  handlers,
+  listenOptions,
+  wait,
+}: {
+  createOptions?: {}
+  handlers?: {}
+  listenOptions?: {}
+  wait?: boolean
+}): Promise<import('http2').Http2Server>
+export function https2({
+  createOptions,
+  handlers,
+  listenOptions,
+  wait,
+}: {
+  createOptions?: {}
+  handlers?: {}
+  listenOptions?: {}
+  wait?: boolean
+}): Promise<import('http2').Http2SecureServer>
+export function tcp({
+  createOptions,
+  handlers,
+  listenOptions,
+  wait,
+}: {
+  createOptions?: {}
+  handlers?: {}
+  listenOptions?: {}
+  wait?: boolean
+}): Promise<import('net').Server>
+export function tls({
+  createOptions,
+  handlers,
+  listenOptions,
+  wait,
+}: {
+  createOptions?: {}
+  handlers?: {}
+  listenOptions?: {}
+  wait?: boolean
+}): Promise<import('tls').Server>
+export function udp({
+  createOptions,
+  handlers,
+  listenOptions,
+  wait,
+}: {
+  createOptions?: {}
+  handlers?: {}
+  listenOptions?: {}
+  wait?: boolean
+}): Promise<import('dgram').Socket>
